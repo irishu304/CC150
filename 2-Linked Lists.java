a4088ed85454fb251f441f8f6ac425718c4c3a10
irishu304 <hulanlan@hulanlan-MacBook-Pro.local>
2017-04-12 11:37:32 -0400

    2.4

--- 2-Linked Lists.java
+++ 2-Linked Lists.java
@@ -38,7 +38,7 @@ void deleteDups(LinkedListNode head) {
 //O(1) space, O(n^2) time
 
 //2.2 Return Kth to Last
-//Recursive
+// Recursive???
 class Index {
 	public int value = 0;
 }
@@ -58,6 +58,96 @@ LinkedListNode klast(LinkedListNode head, int k, Index idx) {
 	return node;
 }
 
+//Iterative
+LinkedListNode kthToLast(LinkedListNode head, int k) {
+	LinkedListNode p1 = head;
+	LinkedListNode p2 = head;
+	//move p1 k nodes into the list
+	for (int i = 0; i < k; i++) {
+		if (p1 == null) return null;
+		//out of bound
+		p1 = p1.next;
+	}
+	//move p1&p2 at same pace
+	while (p1 != null) {
+		p1 = p1.next;
+		p2 = p2.next;
+	}
+	return p2;
+}
+
+//2.3 Delete Middle Node
+boolean delete(LinkedListNode n) {
+	if (n == null || n.next == null)
+		return false; //failure
+	LinkedListNode next = n.next;
+	n.data = next.data;
+	n.next = next.next;
+	return true;
+}
+
+//2.4 Partition
+//two lists: before&after, then merge???
+LinkedListNode partition(LinkedListNode node, int x) {
+	LinkedListNode beforeStart = null;
+	LinkedListNode beforeEnd = null;
+	LinkedListNode afterStart = null;
+    LinkedListNode afterEnd = null;
+
+    while (node != null) {
+    	LinkedListNode next = node.next;
+    	node.next = null; //partite a single node
+    	if (node.data < x) {
+    		if (beforeStart == null) {
+    			beforeStart = node;
+    			beforeEnd = beforeStart;
+    		}
+    		else {
+    			beforeEnd.next = node;
+    			beforeEnd = node;
+    		}
+    	}
+    	else {
+    		if (afterStart == null) {
+    			afterStart = node;
+    			afterEnd = afterEnd;
+    		}
+    		else {
+    			afterEnd.next = node;
+    			afterEnd = node;
+    		}
+    	}
+    	node = next;
+    }
+
+    if (beforeStart == null) 
+    	return afterStart;
+    else {
+    	beforeEnd.next = afterStart;
+    	return beforeStart;
+    }
+}
+
+//less insert at head, greater insert at tail
+LinkedListNode partition(LinkedListNode node, int x) {
+	LinkedListNode head = node;
+	LinkedListNode tail = node;
+
+	while (node != null) {
+		LinkedListNode next = node.next;
+		if (node.data < x) {
+			head.next = node;
+			head = node;
+		}
+		else {
+			node.next = tail;
+			tail = node;
+		}
+		node = next;
+	}
+	tail.next = null;
+	return head;
+}
 
 
 
