d8ebed4fcd70c0c810c618abb14e64ceb51df94b
irishu304 <hulanlan@hulanlan-MacBook-Pro.local>
2017-05-06 17:06:47 -0400

    3.2

--- 3-Stack&Queue.java
+++ 3-Stack&Queue.java
@@ -1,4 +1,4 @@
-//Three in one
+//3.1Three in One
 //fixed division
 class FixedMultiStack {
 	private int numberOfStacks;
@@ -18,7 +18,7 @@ class FixedMultiStack {
 		if (isFull(stackNum)) {
 			throw new FullStackException();
 		} 
-        //increment stack pointer and update top value
+        //increase size of stack stackNum and update top value
 		sizes[stackNum]++;
 		values[indexOfTop(stackNum)] = value;
 	}
@@ -55,6 +55,63 @@ class FixedMultiStack {
 	private int indexOfTop(int stackNum) {
 		int offset = stackNum * stackCapacity;
 		int size = sizes[stackNum];
-		return offset + size - 1;
+		return offset + size - 1; //top of stack stackNum
 	}
 }
+
+//flexible division？？？
+public class multiStack {
+	private class stackInfo {
+		public int start, size, capacity;
+		public stackInfo(int start, int capacity) {
+			this.start = start;
+			this.capacity = capacity;
+		}
+
+		//check if an index on the full array is within stack boundries
+		public boolean isWithinStackCapacity(int index) {
+			if (index < 0 || index >= values.length) return false;
+			//if index wraps around, adjust it
+			int contiIndex = index < start ? index + values.length : index;
+			int end = start + capacity;
+			return start <= contiIndex && contiIndex < end;
+		}
+
+	}
+}
+
+//3.2 Stack Min
+public class StackWithMin extends Stack<NodeWithMin> { //inherit
+	public void push(int value) {
+		int newMin = Math.min(value, min());
+		super.push(new NodeWithMin(value, newMin));
+	}
+
+	public int min() {
+		if (this.isEmpty()) {
+			return Integer.MAX_VALUE; //error value
+		}
+		else {
+			return peek().min;
+		}
+	}
+}
+
+class NodeWithMin {
+	public int value;
+	public int min;
+	public NodeWithMin(int v, int min) {
+		value = v;
+		this.min = min;
+	}
+}
+
+//use an additional stack to track mins
+
+
+
+
+
+
+
+
