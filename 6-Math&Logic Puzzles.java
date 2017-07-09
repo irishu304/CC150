038e6f4e0c377bbc75e692c0befdad2687e14a58
irishu304 <hulanlan@hulanlan-MacBook-Pro.local>
2017-07-09 11:54:44 -0700

    6.10

--- 6-Math&Logic Puzzles.java
+++ 6-Math&Logic Puzzles.java
@@ -52,18 +52,45 @@ double runNFamilies(int n) {
  	return egg2 > floor ? -1 : egg2;
  }
 
+//6.10 Poison
+int findPoison(ArrayList<Bottle> bottles, ArrayList<TestStrip> strips) {
+	runTests(bottles, strips);
+	ArrayList<Integer> positive = getPositive(strips, 7);
+	return setBits(positive);
+}
 
+void runTests(ArrayList<Bottle> bottles, ArrayList<TestStrip> testStrips) {
+	for (Bottle bottle : bottles) {
+		int id = bottle.getID();
+		int bitIndex = 0;
+		while (id > 0) {
+			if ((id & 1) == 1) {
+				testStrips.get(bitIndex).addDrop(0, bottle);
+			}
+			bitIndex++;
+			id >>= 1; //move to next bit, then check
+		}
+	}
+}
 
+ArrayList<Integer> getPositive(ArrayList<TestStrip> testStrips, int day) {
+	ArrayList<Integer> positive = new ArrayList<Integer>();
+	for (TestStrips testStrip : testStrips) { //for each test strip
+		int id = testStrip.getID();
+		if (testStrip.isPositive(day)) {
+			positive.add(id); //mark positive test strip's id
+		}
+	}
+	return positive; //map from arraylist to binary integer
+}
 
+int setBits(ArrayList<Integer> positive) {
+	int id = 0;
+	for (Intger bitIndex : positive) {
+		id |= 1 << bitIndex; //ith bit is set to 1
+	}
+	return id;
+}
 
-
-
-
-
-
-
-
-
-
-
-
+class Bottle {}
+class TestStrip {}
