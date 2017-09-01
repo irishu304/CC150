d3e9228d2403983ad958f71c0778c9f24dfc42ac
irishu304 <hulanlan@hulanlan-MacBook-Pro.local>
2017-08-31 18:04:11 -0700

    7.6

--- 7-Object-Oriented Design.java
+++ 7-Object-Oriented Design.java
@@ -157,10 +157,199 @@ public class ParkingLot {...}
 public class Level {...}
 public class ParkingSpot {...}
 
+//7.5 Online Book Reader
+public class onlineSystem {
+	private UserManager usermanager;
+	private Library library;
+	private Display display;
+	private User activeUser;
+	private Book activeBook;
 
+	public onlineSystem() {
+		usermanager = new UserManager();
+		library = new Library();
+		display = new Display();
+	}
+	public UserManager getUserManager() {return usermanager;}
+	public Library getLibrary() {return library;}
+	public Display getDisplay() {return display;}
 
+	public Book getActiveBook() {return activeBook;}
+	public void setActiveBook(Book book) {
+		activeBook = book;
+		display.displayBook(book);
+	}
 
+	public User getActiveUser() {return activeUser;}
+	public void setActiveUser(User user) {
+		activeUser = user;
+		display.displayUser(user);
+	}
+}
 
+public class Library {
+	private HashMap<Integer, Book> books; //<id, book>
+	public Book addBook(int id, String details) {
+		if (books.containsKey(id)) return null;
+		Book book = new Book(id, details);
+		books.put(id, book);
+		return book;
+	}
+	public boolean remove(Book b) {
+		return remove(b.getID());
+	}
+	public boolean remove(int id) {
+		if (!books.containsKey(id)) return false;
+		books.remove(id);
+		return true;
+	}
+	public Book find(int id) {
+		return books.get(id);
+	}
+}
+
+public class UserManager {
+	private HashMap<Integer, User> users; //<id, user>
+	public User addUser(int id, String details, int accountType) {
+		if (users.containsKey(id)) return null;
+		User user = new User(id, details, accountType);
+		users.put(id, user);
+		return user;
+	}
+	public boolean remove(User u) {
+		return remove(u.getID());
+	}
+	public boolean remove(int id) {
+		if (!users.containsKey(id)) return false;
+		users.remove(id);
+		return true;
+	}
+	public User find(int id) {
+		return user.get(id);
+	}
+}
+
+public class Display {
+	private Book activeBook;
+	private User activeUser;
+	private int pageNumber = 0;
+
+	public void displayUser(User user) {
+		activeUser = user;
+		refreshUsername();
+	}
+	public void displayBook(Book book)b{
+		activeBook = book;
+		pageNumber = 0;
+		refreshTitle();
+		refreshDetails();
+		refreshPage();
+	}
+	public void forwardPage() {
+		pageNumver++;
+		refreshPage();
+	}
+	public void backwardPage() {
+		pageNumber--;
+		refreshPage();
+	}
+	public void refreshUsername();
+	public void refreshTitle();
+	public void refreshDetails();
+	public void refreshPage();
+}
+
+public class Book {
+	private int bookID;
+	private String details;
+	public Book(int id, String det) {
+		bookID = id;
+		details = det;
+	}
+	public int getID() {return bookID;}
+	public void setID(int id) {bookID = id;}
+	public String getDetails() {return details;}
+	public void setDetails(String det) {details = det;}
+}
+
+public class User {
+	private int userID;
+	private String details;
+	private int accountType;
+	public User(int id, String det, int type) {
+		userID = id;
+		this.details = det;
+		this.accountType = type; //???
+	}
+
+	public int getID() {return userID;}
+	public void setID(int id) {userID = id;}
+	public String getDetails() {return details;}
+	public void setDetails(String det) {this.details = det;}
+	public int getAccountType() {return accountType;}
+	public void setAccountType(int t) {accountType = t;}
+
+	public void renewMembership();
+}
+
+//7.6 Jigsaw
+public enum Orientation {
+	LEFT, TOP, RIGHT, BOTTOM;
+	public Orientation getOpposite() {
+		switch(this) {
+			case LEFT: return RIGHT;
+			case RIGHT: return LEFT;
+			case TOP: return BOTTOM;
+			case BOTTOM: return TOP;
+		}
+	}
+}
+
+public enum Shape {
+	INNER, OUTER, FLAT;
+	public Shape getOpposite() {
+		switch(this) {
+			case INNER: return OUTER;
+			case OUTER: return INNER;
+			default: return null;
+		}
+	}
+}
+
+public class Edge {
+	private Shape shape;
+	private Piece parentPiece;
+	public Edge(Shape shape) {...}
+	public boolean fitsWith(Edge edge) {...}
+}
+
+public class Piece {
+	private HashMap<Orientation, Edge> edges = new HashMap<>();
+	public Piece(Edge[] edgeLists) {...}
+	public boolean isBorder() {...}
+	public boolean isCorner() {...}
+	public void rotateEdge(int rotateNum) {...}
+}
+
+public class Puzzle {
+	private LinkedList<Piece> pieces; //remaining pieces
+	private Piece[][] solution; //position to put the piece
+	private int size;
+	public Puzzle(LinkedList<Piece> pieces, int size) {...}
+
+	//Put piece into the solution, turn it appropriately, remove from list
+	private void setEdgeInSolution(LinkedList<Piece> pieces, Edge edge,
+								   int row, int col, Orientation orit) {
+		Piece piece = edge.getParentPiece();
+		piece.setEdgeAsOrientation(edge, orientation);
+		pieces.remove(piece);
+		solution[row][col] = piece;
+	}
+
+	//Find the matching piece in search and insert it
+	private boolean fitNextEdge(LinkedList<Piece> search,int row, int col);
+	public boolean solve();
+}
 
 
 
